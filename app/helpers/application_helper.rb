88f17ff4e888d33ab62c0a6283352e539c8fc1ba
cecilia <cecilia.lourenco@contavoltz.com>
2022-04-14 17:59:34 -0300

    first commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+module ApplicationHelper
+  def locale
+    I18n.locale == :en ? "Inglês Americano" : "Português do Brasil"
+  end
+
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def nome_aplicacao
+    "CRYPTO WALLET APP"
+  end
+
+  def ambiente_rails
+    Rails.env.production? ? "produção" : "desenvolvimento"
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# Use o rails dev:setup para criar o banco de dados e popula-lo com os dados de moedas e tipos de mineração.
+# (lib/tasks/dev.rake)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
